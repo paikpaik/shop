064277780d2082d9416658aa72b15a3da724e396
paikpaik <woduf9311@gmail.com>
2023-10-14 17:44:28 +0900

    Fix: endpoint 수정

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -10,7 +10,7 @@ router.get("/", productController.getProducts);
 router.get("/:productId", productController.getProduct);
 
 // 상품검색(메인페이지)
-router.get("/search", productController.searchProducts);
+router.get("/search/keyword", productController.searchProducts);
 
 // 카테고리별상품조회(메인페이지)
 router.get("/category", productController.categoryProducts);
