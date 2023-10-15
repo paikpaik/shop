3fa47e57a3820cdac473de76a9f2dc8d43efa733
paikpaik <woduf9311@gmail.com>
2023-10-15 19:01:09 +0900

    Test: UserService findById test

--- services/userService.test.js
+++ services/userService.test.js
@@ -8,7 +8,14 @@ describe("UserService", () => {
     UserRepository = {
       findByEmail: async (email) => {
         if (email === "test@test.com") {
-          return { id: 1, name: "test", email: "test@test.com" };
+          return { userId: 1, name: "test", email: "test@test.com" };
+        }
+        return null;
+      },
+
+      findById: async (userId) => {
+        if (userId === 1) {
+          return { userId: 1, name: "test", email: "test@test.com" };
         }
         return null;
       },
@@ -20,7 +27,7 @@ describe("UserService", () => {
     const email = "test@test.com";
     const result = await userService.getUserByEmail(email);
     expect(result).toEqual({
-      id: 1,
+      userId: 1,
       name: "test",
       email: "test@test.com",
     });
@@ -29,6 +36,22 @@ describe("UserService", () => {
   test("email에 해당하는 data가 없으면 null을 리턴", async () => {
     const email = "nonExist@test.com";
     const result = await userService.getUserByEmail(email);
+    expect(result).toBeNull();
+  });
+
+  test("userId 해당하는 data가 있으면 객체를 리턴", async () => {
+    const userId = 1;
+    const result = await userService.getUserById(userId);
+    expect(result).toEqual({
+      userId: 1,
+      name: "test",
+      email: "test@test.com",
+    });
+  });
+
+  test("userId 해당하는 data가 없으면 null을 리턴", async () => {
+    const userId = 9999;
+    const result = await userService.getUserById(userId);
     expect(result).toBeNull();
   });
 });
